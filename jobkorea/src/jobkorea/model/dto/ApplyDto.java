87426e1b5fa2e656264a7e0a9d08b057a275ca3f
minwoo817 <capzhang123@naver.com>
2025-01-10 18:01:04 +0900

    DTO

---
+++ jobkorea/src/jobkorea/model/dto/ApplyDto.java
@@ -0,0 +1,54 @@
+package jobkorea.model.dto;
+
+public class ApplyDto {
+	private boolean apass;
+	private int pno;
+	private int mno;
+	
+	public ApplyDto() {}
+	
+
+	public ApplyDto(boolean apass, int pno, int mno) {
+		super();
+		this.apass = apass;
+		this.pno = pno;
+		this.mno = mno;
+	}
+
+
+	public void setApass(boolean apass) {
+		this.apass = apass;
+	}
+
+
+	public boolean isApass() {
+		return apass;
+	}
+
+
+	public int getPno() {
+		return pno;
+	}
+
+
+	public void setPno(int pno) {
+		this.pno = pno;
+	}
+
+
+	public int getMno() {
+		return mno;
+	}
+
+
+	public void setMno(int mno) {
+		this.mno = mno;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ApplyDto [apass=" + apass + ", pno=" + pno + ", mno=" + mno + "]";
+	}
+	
+}
