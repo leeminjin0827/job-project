00f881389db35fa9bdcc3ab88bb21b48893ad2b7
minwoo817 <capzhang123@naver.com>
2025-01-13 16:16:43 +0900

    공고리스트출력 메소드 추가

--- jobkorea/src/jobkorea/view/ListView.java
+++ jobkorea/src/jobkorea/view/ListView.java
@@ -27,8 +27,18 @@ public class ListView {
 		System.out.println();
 	}
 	// [2] 공고 리스트 출력
-	public void pList() {
+	public void pList() {	
 		ArrayList<HashMap<String , String>> pList = ListController.getInstance().pList();
+		// 로그인된 기업번호 매개변수로 controller , dao 에 전달해 주세용. -> 로그인된 기업 공고만 나와야되니까!
+		System.out.println();
+		for(int i = 0; i < pList.size(); i++) {
+			HashMap<String, String> pDto = pList.get(i);
+			System.out.println("공고번호 : " + pDto.get("공고번호") 
+			  +" \t 공고명 : " + pDto.get("제목") +" 내용 : " + pDto.get("내용")
+			  +" 경력 : " + pDto.get("경력") +" 모집인원 : " + pDto.get("인원")
+			  +" 연봉 : " + pDto.get("연봉") +" 공고시작일 : " + pDto.get("공고시작일")
+			  +" 공고종료일 : " + pDto.get("공고종료일") +" 카테고리 : " + pDto.get("카테고리명") + " 기업명 : " + pDto.get("기업명"));
+		}
 	}
-	
+
 }
