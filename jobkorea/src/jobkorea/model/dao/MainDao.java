683c3ad33b3a455b6985ca40c5a0c04bca1e8053
leeminjin0827 <lmj98202@gmail.com>
2025-01-14 17:01:39 +0900

    jobkorea

--- jobkorea/src/jobkorea/model/dao/MainDao.java
+++ jobkorea/src/jobkorea/model/dao/MainDao.java
@@ -1,20 +1,13 @@
 package jobkorea.model.dao;
 
 import java.sql.Connection;
-
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import jobkorea.model.dto.PostDto;
+
 import jobkorea.model.dto.EnterpriseDto;
 import jobkorea.model.dto.MemberDto;
-import jobkorea.model.dto.ReviewDto;
-
-import jobkorea.controller.MainController;
-
 
 public class MainDao {
 	
@@ -37,89 +30,30 @@ public class MainDao {
 		System.out.println(">> DB 연동 실패 "+ e);
 		} // catch end
 	} // f end
-	public static MainDao getinstance() { return instance; }
+	public static MainDao getInstance() { return instance; }
 	// - 싱글톤
 	
-	// [1] 일반 회원가입 SQL 처리 메소드 
-		public boolean mSignUp(MemberDto memberDto) {
-			try {
-				// SQL 작성
-				
-				String sql = "insert into member(mid, mpwd, mname, mgender, mdate, maddr) values(?, ?, ?, ?, ?, ?)";
-
-				// DB와 연동된 곳에 SQL 기재
-				PreparedStatement ps = conn.prepareStatement(sql);
-				
-				ps.setString(1, memberDto.getMid());
-				ps.setString(2, memberDto.getMpwd());
-				ps.setString(3, memberDto.getMname());
-				ps.setBoolean(4, memberDto.isMgender());
-				ps.setString(5, memberDto.getMdate());
-				ps.setString(6, memberDto.getMaddr());
-						
-				// 기재된 SQL을 실행하고 결과 받기
-				int count = ps.executeUpdate();
-				
-				// 결과에 따른 처리 및 반환
-				if(count == 1) {return true;}
-			}catch(SQLException e) {System.out.println(e);}
-			return false;
-			
-		} // f mSignUp end
-
-    
-		// 2. 일반 회원 SQL 처리 메소드
-		public int mLogin( MemberDto memberDto ) {
-			try {
-				// [1] SQL 작성
-				String sql = "select mno from member where mid = ? and mpwd = ?";
-				// [2] DB와 연동된 곳에 SQL 기재
-				PreparedStatement ps = conn.prepareStatement(sql);
-				// [*] 기재된 SQL 에 매개변수 값 대입
-				ps.setString(1, memberDto.getMid() );
-				ps.setString(2, memberDto.getMpwd());
-				// [3] 기재된 SQL 실행하고 결과 받기
-				ResultSet rs = ps.executeQuery();
-				// [4] 결과에 따른 처리 및 반환
-				if( rs.next() ) {
-					int mno = rs.getInt("mno");
-					return mno;
-				} // if end
-			} // try end
-			catch( SQLException e ) { System.out.println( e ); }
-			return 0;
-		} // f end
- 	
-    // [3] 회원 로그아웃 메소드
-    public void mLogout() {
-    	
-    }
-    
-    
-    
-    // [1] 기업 회원가입 메소드
-	public boolean eSignUp(EnterpriseDto enterpriseDto) {
+	// 2. 일반 회원 SQL 처리 메소드
+	public int mLogin( MemberDto memberDto ) {
 		try {
-			// SQL 작성
-			
-			String sql = "insert into enterprise(eid, epwd, ename, eaddr) values(?, ?, ?, ?)";
-
-			// DB와 연동된 곳에 SQL 기재
+			// [1] SQL 작성
+			String sql = "select mno from member where mid = ? and mpwd = ?";
+			// [2] DB와 연동된 곳에 SQL 기재
 			PreparedStatement ps = conn.prepareStatement(sql);
-			
-			ps.setString(1, enterpriseDto.getEid());
-			ps.setString(2, enterpriseDto.getEpwd());
-			ps.setString(3, enterpriseDto.getEname());
-			ps.setString(4, enterpriseDto.getEaddr());
-					
-			// 기재된 SQL을 실행하고 결과 받기
-			int count = ps.executeUpdate();
-			
-			// 결과에 따른 처리 및 반환
-			if(count == 1) {return true;}
-		}catch(SQLException e) {System.out.println(e);}
-		return false;
-	}
+			// [*] 기재된 SQL 에 매개변수 값 대입
+			ps.setString(1, memberDto.getMid() );
+			ps.setString(2, memberDto.getMpwd());
+			// [3] 기재된 SQL 실행하고 결과 받기
+			ResultSet rs = ps.executeQuery();
+			// [4] 결과에 따른 처리 및 반환
+			if( rs.next() ) {
+				int mno = rs.getInt("mno");
+				return mno;
+			} // if end
+		} // try end
+		catch( SQLException e ) { System.out.println( e ); }
+		return 0;
+	} // f end
 	
 	// 2 기업 회원 SQL 처리 메소드
 	public int eLogin( EnterpriseDto enterpriseDto ) {
@@ -142,68 +76,5 @@ public class MainDao {
 		catch( SQLException e ) { System.out.println( e ); }
 		return 0;
 	} // f end
- 	
- 	
-    // [3] 기업 로그아웃 메소드
-    public void eLogout() {
-       
-    }
-    
-    
-    // [1] 우수기업 R
-    public ArrayList<HashMap<String, String>>  bestList() {
-    	ArrayList<HashMap<String, String>> bList = new ArrayList<>();
-    	try {
-    		String sql = "select e.ename , avg(r.rrating) as ravg from review r left join enterprise e "
-    					+ "on  r.eno = e.eno group by r.eno order by ravg desc";
-    		PreparedStatement ps = conn.prepareStatement(sql);
-    		ResultSet rs = ps.executeQuery();
-    		
-    		while(rs.next()) {
-    			String list = rs.getString("ename");
-    			String rating = rs.getString("ravg");
-    			
-    			HashMap<String, String> map = new HashMap<String, String>();
-    			
-    			map.put("기업명", list);
-    			map.put("별점", rating);
-    			
-    			bList.add(map);
-    		}
-    	}catch (Exception e) {
-    		System.out.println(e);
-		}
-    	
-    	return bList;
-    }
-    // [2] 후기 R
-    public ArrayList<HashMap<String, String>>  reviewList(String ename) {
-    	ArrayList<HashMap<String, String>> rList = new ArrayList<>();
-		try {
-			String sql = "select e.ename,  r.rcontent, r.rrating from review r join enterprise e "
-						+ "on r.eno = e.eno  where e.ename = ?";
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, ename);
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				String name = rs.getString("ename");
-				String rcontent = rs.getString("rcontent");
-				String rrating = rs.getString("rrating");
-				HashMap<String, String> map = new HashMap<String, String>();
-				
-				map.put("기업명", name);
-				map.put("후기", rcontent);
-				map.put("별점", rrating);
-				
-				rList.add(map);
-			}
-			
-		}catch (SQLException e) {
-			System.out.println(e);
-		}
-		
-		return rList;
-	}
-}
->>>>>>> branch 'kimdoha' of https://github.com/leeminjin0827/job-project
+	
+} // c end
