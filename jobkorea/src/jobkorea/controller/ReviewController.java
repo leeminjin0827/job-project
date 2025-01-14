f329ec455c917893d065e92c6cdca9551dc4b774
ithodol <ddmdjesz1@gmail.com>
2025-01-14 18:18:22 +0900

    리뷰 파일

--- jobkorea/src/jobkorea/controller/ReviewController.java
+++ jobkorea/src/jobkorea/controller/ReviewController.java
@@ -1,38 +1,5 @@
 package jobkorea.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-import jobkorea.model.dao.ReviewDao;
-import jobkorea.model.dto.ReviewDto;
-
 public class ReviewController {
-	// 싱글톤
-	private static ReviewController instance = new ReviewController();
-	private ReviewController() {}
-	public static ReviewController getInstance() { return instance; }
-	
-	
-	public boolean rWrite(ReviewDto reviewDto, String rname, int loginMno) {
-		boolean result = ReviewDao.getInstance().rWrite(reviewDto, rname, loginMno);
-		return result;
 
-	}
-	
-	
-	public ArrayList<HashMap<String, String>> rPassPrint(int mno) {
-		ArrayList<HashMap<String, String>> passList = ReviewDao.getInstance().rPassPrint(mno);
-		return passList;
-		
-	}
-	
-	
-	
-	public ArrayList<HashMap<String, String>> rPrintAll(int mno) {
-		ArrayList<HashMap<String, String>> printList = ReviewDao.getInstance().rPrintAll(mno);
-	}
-	
-	
-
-	
 }
